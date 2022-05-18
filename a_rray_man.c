50cf307e00cbb35072512e9882c071b6b12af45c
Virgil Surin <virgil.surin@student.umons.ac.be>
2022-05-18 16:26:26 +0200

    added comment and deleted useless code

--- a_rray_man.c
+++ a_rray_man.c
@@ -28,6 +28,7 @@ int tab_size = sizeof(signed int)*65536;
 signed int* shared_mem;
 
 signed int* create_table(){
+    /* we randomly generate a table */
     // Use current time as seed for random generator
     srand(time(0));
 
@@ -39,40 +40,8 @@ signed int* create_table(){
     return table;
 }
 
-int wr_table(){
-    signed int* table = create_table();
-
-    key_t ipc_key = ftok("malenia", 42);
-    if (ipc_key == -1){
-        perror("ftok");
-        return EXIT_SUCCESS;
-    }
-
-    int shmid = shmget(ipc_key, 65536*sizeof(signed int) + 2*sizeof(pid_t), IPC_CREAT | 0666);
-    if (shmid == -1){
-        perror("shmget");
-        return EXIT_FAILURE;
-    }
-
-    shared_mem = shmat(shmid, NULL, 0);
-    if (shared_mem == (void*) - 1)
-    {
-        perror("shmat");
-        return EXIT_FAILURE;
-    }
-    else{
-        for(int i = 0; i<65536; i++){
-            
-            shared_mem[i] = table[i];
-        }
-    }
-
-    shmdt( shared_mem );
-    return EXIT_SUCCESS;
-}
-
-
 void handle_done(int signum, siginfo_t* info, void* context) {
+    /* time to check if the array is correctly sorted */
     state = PROCESSING;
     
     /* re-read in the shared memory */
@@ -93,7 +62,7 @@ void handle_done(int signum, siginfo_t* info, void* context) {
         perror("shmat");
         /* return EXIT_FAILURE; */
     }
-    
+    /* sort verification */
     for (int i = 0; i < 65535; i++)
     {
         if (!(shared_mem[i] < shared_mem[i+1]))
@@ -121,18 +90,21 @@ int main(int argc, char const *argv[]) {
     descriptor.sa_flags = SA_SIGINFO;
     descriptor.sa_sigaction = handle_done;
     sigaction(SIGRT_DONE, &descriptor, NULL);
-    
+
+    /* we try to get access to the sem */
     sem_t* sema = NULL;
-    do{
-        printf("%d is trying to get sema\n", getpid());
+    printf("%d is trying to get sema\n", getpid());
+    do {
         sema = sem_open("/memory_semaphore" , O_CREAT, 0600, 1);
-    }while (sema == SEM_FAILED);
-    sem_wait(sema); //useless here because semaphore cannot be created if another process is already working anyway? */
-    state = PROCESSING;
+    } while (sema == SEM_FAILED);
+    sem_wait(sema);             /* lock it only for us */
     printf("%d recieved sema\n", getpid());
+    
     /* start of write table */
+    state = PROCESSING;    
     signed int* table = create_table();
 
+    /* we access the shared memory */
     key_t ipc_key = ftok("./malenia", 42);
     if (ipc_key == -1){
         perror("ftok");
@@ -151,23 +123,23 @@ int main(int argc, char const *argv[]) {
         perror("shmat");
         return EXIT_FAILURE;
     }
-    else{
-        int* tab = shared_mem + 2*sizeof(pid_t);
-        for(int i = 0; i<65536; i++){
-            
-            tab[i] = table[i];
-        }
+    /* we write the generated table into the shared memory */
+    int* tab = shared_mem + 2*sizeof(pid_t);
+    for(int i = 0; i<65536; i++){
+        tab[i] = table[i];
     }
     /* end of write table */
     pid_t master_pid = shared_mem[0];
     shared_mem[1] = getpid();
     shmdt(shared_mem);
     
-    /* process done, send signal */
+    /* the tab is generated and sent into the shared memory,
+       we send a signal to the master */
     union sigval envelope;
     printf("%d is sending ok to %d\n", getpid(), master_pid);
     sigqueue(master_pid, SIGRT_OK, envelope);
-    /* back to waiting */
+    
+    /* we wait for the master to process the array */
     state = WAITING;
     
     while (state == WAITING) {
